b4a4b445f78babcaba8a9cf94d429e2a638aa4dc
arjun391c <arjun391c@gmail.com>
2020-06-18 12:24:14 +0530

    final

--- app/auth.js
+++ app/auth.js
@@ -24,7 +24,7 @@ module.exports = function (app, db) {
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "https://messenger-19.herokuapp.com//auth/github/callback"
+        callbackURL: "https://messenger-19.herokuapp.com/auth/github/callback"
       },
       function(accessToken, refreshToken, profile, cb) {
         //   console.log(profile)
